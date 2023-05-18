e590ed80fecd2d47f330b6ed260e0fc8ffb1e17d
Ifeanyichukwu Goodness <dandycute2@gmail.com>
2023-05-18 15:54:08 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - This is the entry point of the program
+ *
+ * Return: Should always be 0 (indicating a successful output)
+ */
+int main(void)
+{
+
+	puts("Programming is like building a multilingual puzzle");
+
+	return (0);
+}
