b10ca4db9e4d659c19b647bfb79d11e6ab62d5ae
Ifeanyichukwu Goodness <dandycute2@gmail.com>
2023-05-18 17:52:25 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio>
+/**
+ * main - This is the point of entry
+ *
+ * Return: It's always 0 (showing success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n};
+
+	return (0);
+}
