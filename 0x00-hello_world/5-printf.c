51640b7d535a4d639f8ef40994b95257ceb65291
Ifeanyichukwu Goodness <dandycute2@gmail.com>
2023-06-08 17:39:51 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,11 @@
 /**
  * main - This is the point of entry
  *
+ * Print "with proper grammar, but the outcome is a piece of art,"
+ *
  * Return: It's always 0 (showing success)
  */
+
 int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n");
